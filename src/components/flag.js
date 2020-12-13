3ca7a20d31fe16fa4c4e00c9be08a2c5b229cd37
Carwin Young <carwinyoung@gmail.com>
2020-12-12 22:50:24 -0600

    Updating Flag component.

--- src/components/flag.js
+++ src/components/flag.js
@@ -1,39 +1,72 @@
 import React from 'react';
-
 import flagGen from 'flag-generator';
 
 // @todo: modify flagGenerator() to accept an element or an ID to hook it's canvas on.
 class Flag extends React.Component {
-// const Flag = () => {
   constructor(props) {
     super(props);
-    this.state = {seed: null}
+
+    const seed = props.seedString !== '' ? flagGen.Utilities.generateSeed(props.seedString) : flagGen.Utilities.generateSeed();
+
+    this.state = {
+      seed: seed,
+      flag: new flagGen.Flag("flag__canvas-container", "3:5"),
+    };
+
   }
-  static getDerivedStateFromProps(props, state) {
-    console.log('About to mount');
-    if (props.seedString !== '') {
-      flagGen.Utilities.generateSeed(props.seedString);
-    } else {
-      flagGen.Utilities.generateSeed();
+
+  getDivisionInfo(divisionIndex) {
+    const getSubDivisionInfo = (subDivisionIndex) => {
+      const subDivisionInfo = Object.keys(this.state.flag.divisions[divisionIndex][subDivisionIndex]).map((val, index) => {
+        return <li key={index+'_'+val}>{`${val}: ${this.state.flag.divisions[divisionIndex][subDivisionIndex][val]}`}</li>
+      });
+      return subDivisionInfo;
     }
-  }
-  getFlagSeedFromSettings() {
-    return flagGen.settings.seed;
-  }
-  componentDidMount() {
-    console.log('Did mount');
-    this.setState({
-      seed: flagGen.settings.seed
+    const divisionInfo = Object.keys(this.state.flag.divisions[divisionIndex]).map((val, index) => {
+      if (typeof this.state.flag.divisions[divisionIndex][val] === 'object') {
+        const subDivisionInfo = getSubDivisionInfo(val);
+        return <li key={index+'_'+val}>{val}:<ul>{subDivisionInfo}</ul></li>
+      } else {
+        if (val !== 'seed' && val !== 'seedMultiplier') {
+          return <li key={index+'_'+val}>{`${val}: ${this.state.flag.divisions[divisionIndex][val]}`}</li>
+        }
+      }
     });
-    flagGen.flagGenerator("flag__canvas-container", this.state.seed);
+
+    return divisionInfo;
   }
+
+  componentDidMount() {
+    // Draw the flag after the component has mounted.
+    this.state.flag.drawFlag();
+  }
+
   render() {
+    const self = this;
+    const divisionParams = this.state.flag.divisions.map((val, index) =>
+        <li key={index+'_'+val}>{this.state.flag.divisions[index].constructor.name}
+        <ul>{self.getDivisionInfo(index)}</ul>
+        </li>
+    );
+
     return (
-        <div className="flag">
+      <div className="flag">
         <div className="flag__seed">
-        Seed string <strong>"{this.props.seedString}"</strong> generated seed: <strong>{this.state.seed}</strong></div>
-          <div id="flag__canvas-container" />
+          Seed string <strong>"{this.props.seedString}"</strong> generated seed: <strong>{this.state.seed}</strong>
         </div>
+        <div id="flag__canvas-container" />
+
+        <div className="flag__attributes">
+          <ul>
+            <li>Seed string: {this.props.seedString || 'random'}</li>
+            <li>Generated seed: {this.state.seed}</li>
+            <li>Aspect ratio: {this.state.flag.aspect.h}:{this.state.flag.aspect.w}</li>
+            <li>Dimensions: {this.state.flag.dimensions.h}x{this.state.flag.dimensions.w}</li>
+            <li># of Divisions: {this.state.flag.divisionCount}</li>
+            {divisionParams}
+          </ul>
+        </div>
+      </div>
     );
   }
 
--- src/index.js
+++ src/index.js
@@ -4,21 +4,9 @@ import { render } from 'react-dom';
 // import flagGen from 'flag-generator';
 import Flag from './components/flag.js';
 
-// let seed = flagGen.generateSeed('carwin young is great and he is nice');
-// let seed = flagGen.generateSeed('carwin young is great and he is good at making flags');
-// console.log(flagGen);
-// let seed = flagGen.Utilities.generateSeed('carwin');
-
-
-// // let seed = flagGen.generateSeed()
-// console.log('Now we have a seed, let\'s send it to the flagGenerator function: ', seed);
-// console.log('The project thinks the seed from the library is: ', flagGen.settings.seed);
-
-// flagGen.flagGenerator(flagGen.settings.seed);
-
 function App() {
   const [state, setState] = useState("ClickMe");
-  return <Flag seedString="why are crosses so unpopular" />
+  return <Flag seedString="" />
 }
 
 render(<App />, document.getElementById("root"));
